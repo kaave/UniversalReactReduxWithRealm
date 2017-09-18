13bbae2135a5f82e9057af5f8aa8abbaa8b8ef9c
kaave <abe@framelunch.jp>
2017-09-18 18:14:26 +0900

    Move create CategoriesEntiry method to server side

--- source/epics/categories.ts
+++ source/epics/categories.ts
@@ -5,48 +5,12 @@ import { Action } from 'redux';
 import { combineEpics, createEpicMiddleware, Epic } from 'redux-observable';
 
 import * as Categories from '../actions/categories';
-import { CategoryEntity } from '../entities/category';
 import { CategoriesEntity } from '../entities/categories';
 
 export function getCategoriesEntity() {
   return fetch('http://localhost:3000/api/categories')
     .then(data => data.json())
-    .then((json: CategoryEntity[]) => {
-      return json.reduce((tempData, { path, count }) => {
-        const pathTree = path.split('/');
-        let parentInfo = tempData;
-
-        // initialize
-        pathTree.forEach((pathDetail, index) => {
-          let currentInfo = parentInfo.children.find(info => info.path === pathDetail);
-          if (!currentInfo) {
-            currentInfo = {
-              path: pathDetail,
-              count: 0,
-              children: [],
-            };
-
-            parentInfo.children.push(currentInfo);
-          }
-          parentInfo = currentInfo;
-        });
-
-        // calc count
-        const matchRoutes: Array<CategoriesEntity | undefined> = [tempData];
-        for (let i = 0, l = pathTree.length; i < l; i += 1) {
-          const lastMatchRoute = matchRoutes[matchRoutes.length - 1];
-          if (lastMatchRoute) {
-            matchRoutes.push(lastMatchRoute.children.find(route => route.path === pathTree[i]));
-          }
-        }
-        matchRoutes.forEach(route => route && (route.count += count));
-        return tempData;
-      }, {
-        path: '/',
-        count: 0,
-        children: [],
-      } as CategoriesEntity);
-    });
+    .then((json: CategoriesEntity) => json);
 }
 
 const getDoneEpic: Epic<Action, void> = (actions$, store) => actions$.ofAction(Categories.getStart)
--- source/server/data/dataStores/category.ts
+++ source/server/data/dataStores/category.ts
@@ -1,6 +1,7 @@
 import realm from '../../databases/factory';
 import * as Category from '../../databases/schemas/category';
 import { CategoryEntity } from '../../../entities/category';
+import { CategoriesEntity } from '../../../entities/categories';
 
 function parseEntity({ path, createAt, updateAt, knowledges }: Category.Types): CategoryEntity {
   return { path, createAt, updateAt, count: knowledges.length };
@@ -8,5 +9,39 @@ function parseEntity({ path, createAt, updateAt, knowledges }: Category.Types): 
 
 export function get() {
   return realm.objects<Category.Types>(Category.name)
-    .map(parseEntity);
+    .map(parseEntity)
+    .reduce((tempData, { path, count }) => {
+      const pathTree = path.split('/');
+      let parentInfo = tempData;
+
+      // initialize
+      pathTree.forEach((pathDetail, index) => {
+        let currentInfo = parentInfo.children.find(info => info.path === pathDetail);
+        if (!currentInfo) {
+          currentInfo = {
+            path: pathDetail,
+            count: 0,
+            children: [],
+          };
+
+          parentInfo.children.push(currentInfo);
+        }
+        parentInfo = currentInfo;
+      });
+
+      // calc count
+      const matchRoutes: Array<CategoriesEntity | undefined> = [tempData];
+      for (let i = 0, l = pathTree.length; i < l; i += 1) {
+        const lastMatchRoute = matchRoutes[matchRoutes.length - 1];
+        if (lastMatchRoute) {
+          matchRoutes.push(lastMatchRoute.children.find(route => route.path === pathTree[i]));
+        }
+      }
+      matchRoutes.forEach(route => route && (route.count += count));
+      return tempData;
+    }, {
+      path: '/',
+      count: 0,
+      children: [],
+    } as CategoriesEntity);
 }
